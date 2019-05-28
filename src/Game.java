e66c675c801e3871a5c5692625a686d15a6a2f8c
komal.bhatt <komal18@navgurukul.org>
2019-05-28 12:18:01 +0530

    Add.validation for playing continuously.

--- src/Game.java
+++ src/Game.java
@@ -2,30 +2,28 @@ import java.util.*;
 
 public class Game {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your guess word:- ");
-        String userInput = scanner.next();
-        List<String> listOfWords = Arrays.asList("Rock","Paper","Scissors");
-        Random random = new Random();
-        int randomNumber = random.nextInt(listOfWords.size());
-        String computerChoice = listOfWords.get(randomNumber);
-        System.out.println(computerChoice);
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your guess word:- ");
+            String userInput = scanner.next();
+            List<String> listOfWords = Arrays.asList("Rock", "Paper", "Scissors");
+            Random random = new Random();
+            int randomNumber = random.nextInt(listOfWords.size());
+            String computerChoice = listOfWords.get(randomNumber);
+            System.out.println(computerChoice);
 
-        if(computerChoice.equalsIgnoreCase(userInput)){
-            System.out.println("Draw");
+            if (computerChoice.equalsIgnoreCase(userInput)) {
+                System.out.println("Draw");
+            } else if (!computerChoice.equalsIgnoreCase(userInput))
+                if (computerChoice.equalsIgnoreCase("rock") && userInput.equalsIgnoreCase("paper")) {
+                    System.out.println("Win");
+                } else if (computerChoice.equalsIgnoreCase("Scissors") && userInput.equalsIgnoreCase("Rock")) {
+                    System.out.println("Win");
+                } else if (computerChoice.equalsIgnoreCase("paper") && userInput.equalsIgnoreCase("Scissors")) {
+                    System.out.println("Win");
+                } else {
+                    System.out.println("lose");
+                }
         }
-        else if (!computerChoice.equalsIgnoreCase(userInput))
-            if (computerChoice.equalsIgnoreCase("rock") && userInput.equalsIgnoreCase("paper")) {
-                System.out.println("Win");
-            }
-            else if(computerChoice.equalsIgnoreCase("Scissors") && userInput.equalsIgnoreCase("Rock")){
-                System.out.println("Win");
-            }
-            else if(computerChoice.equalsIgnoreCase("paper") && userInput.equalsIgnoreCase("Scissors")){
-                System.out.println("Win");
-            }
-            else{
-                System.out.println("lose");
-            }
     }
 }
