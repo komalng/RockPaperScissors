6d5809b19b05448639f1ef6c43cbffd4825d489f
komal.bhatt <komal18@navgurukul.org>
2019-05-28 10:22:55 +0530

    Add.taking userInput.

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Game {
+    public static void main(String[] args) {
+        Scanner myObject = new Scanner(System.in);
+        System.out.println("Enter your guess word:- ");
+        String userInput = myObject.next();
+    }
+}
